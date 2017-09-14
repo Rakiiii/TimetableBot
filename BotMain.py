1cbdb90c8b4fabcab9156bd520dd95561c0a8661
Rakiiii <31930457+Rakiiii@users.noreply.github.com>
2017-09-14 19:05:59 +0300

    Add files via upload

--- BotMain.py
+++ BotMain.py
@@ -4,12 +4,13 @@ import telebot #телеграмовское апи
 import Config  #файлик с константами
 import urllib  # тырим файлы по урл
 import xlrd,xlwt #апи маломягких для exl
+import pandas
 from difflib import ndiff #на будущее
 
 
 Bot = telebot.TeleBot(Config.Token) #содаем объект бот
-OldTimetable = xlrd.open_workbook(' ' , formatting_info=True) #грузим старое расписание
-NewTimetable = xlrd.open_workbook( ' ' , formatting_info=True) #тырим новое расписание
+#OldTimetable = xlrd.open_workbook(' ' , formatting_info=True) #грузим старое расписание
+#NewTimetable = xlrd.open_workbook( ' ' , formatting_info=True) #тырим новое расписание
 
 @Bot.messege_handler(content_types=['commands'])  #получаем команду и предлагаем выбрать номер группы
 def get_commands(message):
@@ -17,7 +18,8 @@ def get_commands(message):
         Bot.send_message(message.chat.id , "напиши номер группым из доступных")
         i = 0
         for i in range(Config.Groups):
-            Bot.send_message(message.chat.id, Config.GroupsNumber[i] )
+            Groups = Groups + Config.GroupsNumber[i] + ' , '
+        Bot.send_message(message.chat.id, Groups )
 
 
 
@@ -26,12 +28,16 @@ def get_text(message):
     i = 0
     for i in range(Config.Groups):
         if message.text == Config.GroupsNumber[i]:
+            OldTimetableUrl = urllib.urlopen(changeURL(Config.Unn , Config.TimetableNumber - 1))#делаем урл для старого расписания
+            NewTimetableUrl = urllib.urlopen(changeURL(Config.Unn , Config.TimetableNumber))#делаем урл для нового расписания
+            OldTimetable = xlrd.open_workbook(OldTimetableUrl , formatting_info=True)#берем старое расписание
+            NewTimetable = xlrd.open_workbook(NewTimetableUrl , formatting_info=True)#берем новое расписание
             if differens(OldTimetable , NewTimetable) == True:  #проверяем расписание
-                OldTimetable = NewTimetable
+                OldTimetable = NewTimetable #если расписания отличаются ,то берем новое
             timetable[i]
 
 
-def timetable(OldTimetable , Number):                                #должно доставать из базы данных и отсылать расписание
+def timetable(Timetable , Number):                                #должно доставать из базы данных и отсылать расписание
 
     Bot.send_message(message.chat.id ,   )
 
@@ -59,19 +65,7 @@ def differens(OldTimetable ,  NewTimetable):               #сравниваем
 
 
 
-def changeURL(Url):
-    i = 0
-    for i in range(Config.Unn):
-        if (Url[i] == 1) or (Url[i] == 2) or\
-                (Url[i] == 3) or (Url[i] == 4) or (Url[i] == 5)\
-                or (Url[i] == 6) or (Url[i] == 7) or (Url[i] == 8)\
-                or (Url[i] == 9):
-            if (Url[i+1] == 9):
-                Change = int(Url[i])
-                Change += 1
-                Url[i] = str(Change)
-                Url[i+1] = '0'
-            else:
-                Change = int(Url[i+1])
-                Change += 1
-                Url[i+1] = str(Change)
+def changeURL(Url , Number ):
+    Number += 1
+    Url = Url + str(Number) + '.xls'
+    return Url
--- Config.py
+++ Config.py
@@ -2,4 +2,6 @@
 Token = "429185056:AAEj_Erv5wnLjBnhP0nqKKkGsBkahhzLPL8"
 Groups =
 GroupsNumber = [] #превратить в словарь и сопоставить номер группы с номером столбца
-Unn = "http://www.itmm.unn.ru/files/2016/08/Osen_2017_bak-27.xls"
+#Unn = "http://www.itmm.unn.ru/files/2016/08/Osen_2017_bak-27.xls"
+Unn = "http://www.itmm.unn.ru/files/2016/08/Osen_2017_bak-"
+TimetableNumber = 29
